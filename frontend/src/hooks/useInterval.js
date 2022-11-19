7ef6dd11d86479d6da15ce4610770d2aa4c747dd
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-11-18 18:29:43 -0600

    Update useInterval.js

--- frontend/src/hooks/useInterval.js
+++ frontend/src/hooks/useInterval.js
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 
 export default function useInterval(callback, delay) {
   const savedCallback = useRef();
-
+  
   // Remember the latest function.
   useEffect(() => {
     savedCallback.current = callback;
